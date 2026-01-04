bfa6bbd907b37bfbbad546296d6db64950f551f8
federico pfund <federicopfund@gmail.com>
2026-01-04 15:46:38 -0300

    feat(controller): agregar ProfileController con rutas de navegación principal
    Se implementa el ProfileController que expone las acciones home, portfolio,
    publications, blog e ideas, encargadas de renderizar las vistas principales
    del perfil y estructurar la navegación de la aplicación.

---
+++ app/controllers/ProfileController.scala
@@ -0,0 +1,29 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+
+@Singleton
+class ProfileController @Inject()(cc: ControllerComponents)
+  extends AbstractController(cc) {
+
+  def home = Action { implicit request =>
+    Ok(views.html.home())
+  }
+
+  def portfolio = Action { implicit request =>
+    Ok(views.html.portfolio())
+  }
+
+  def publications = Action { implicit request =>
+    Ok(views.html.publications())
+  }
+
+  def blog = Action { implicit request =>
+    Ok(views.html.blog())
+  }
+
+  def ideas = Action { implicit request =>
+    Ok(views.html.ideas())
+  }
+}
